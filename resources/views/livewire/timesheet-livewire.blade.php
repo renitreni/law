cd5e0039eca9b4edc1f488a8d3d9dda6ea993d33
Renier Trenuela <renier.trenuela@gmail.com>
2023-01-26 15:29:25 +0800

    Update timesheet-livewire.blade.php

--- resources/views/livewire/timesheet-livewire.blade.php
+++ resources/views/livewire/timesheet-livewire.blade.php
@@ -1,20 +1,23 @@
 <div>
-<div id="calendar"></div>
+    <div id="calendar"></div>
 </div>
 
 
 @push('scripts')
-<script src="{{ asset('fullcalendar/dist/index.global.min.js') }}"></script>
-<link href="" rel="stylesheet" />
-<script>
-
-    document.addEventListener('DOMContentLoaded', function() {
-      var calendarEl = document.getElementById('calendar');
-      var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth'
-      });
-      calendar.render();
-    });
-
-  </script>
+    <script src="{{ asset('fullcalendar/dist/index.global.min.js') }}"></script>
+    <link href="" rel="stylesheet" />
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                headerToolbar: {
+                    start: 'dayGridMonth,timeGridWeek,timeGridDay',
+                    center: 'title',
+                    end: 'prevYear,prev,next,nextYear'
+                },
+            });
+            calendar.render();
+        });
+    </script>
 @endpush
