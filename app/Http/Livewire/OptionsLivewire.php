cd1df6112c759b9f87d72bc72514c2696b4440ea
Renier Trenuela <renier.trenuela@gmail.com>
2023-02-01 01:04:10 +0800

    Update OptionsLivewire.php and options-livewire.blade.php

--- app/Http/Livewire/OptionsLivewire.php
+++ app/Http/Livewire/OptionsLivewire.php
@@ -9,7 +9,12 @@ use Livewire\Component;
 class OptionsLivewire extends Component
 {
     public mixed $client = [];
+
     public mixed $matter = [];
+
+    public $clientDetails = [];
+
+    public $matterDetails = [];
 
     public function render()
     {
@@ -17,5 +22,41 @@ class OptionsLivewire extends Component
         $this->matter = Matter::all();
 
         return view('livewire.options-livewire');
+    }
+
+    public function storeClient()
+    {
+        $this->validate([
+            'clientDetails.name' => 'required',
+            'clientDetails.code' => 'required|unique:clients,code',
+        ]);
+        Client::create([
+            'name' => $this->clientDetails['name'],
+            'code' => $this->clientDetails['code'],
+        ]);
+        $this->clientDetails = [];
+    }
+
+    public function storeMatter()
+    {
+        $this->validate([
+            'matterDetails.name' => 'required',
+            'matterDetails.code' => 'required|unique:matters,code',
+        ]);
+        Matter::create([
+            'name' => $this->matterDetails['name'],
+            'code' => $this->matterDetails['code'],
+        ]);
+        $this->matterDetails = [];
+    }
+
+    public function destroyClient($id)
+    {
+        Client::destroy($id);
+    }
+
+    public function destroyMatter($id)
+    {
+        Matter::destroy($id);
     }
 }
--- resources/views/livewire/options-livewire.blade.php
+++ resources/views/livewire/options-livewire.blade.php
@@ -1,47 +1,60 @@
 <div>
     <div class="row">
-        <div class="col-6">
+        <div class="col-6 d-flex flex-column">
+            <h4>Client</h4>
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Code"
+                <input type="text" class="form-control" wire:model="clientDetails.code" placeholder="Code"
                     aria-label="Code" aria-describedby="button-addon2">
-                    <input type="text" class="form-control" placeholder="Name"
-                        aria-label="Name" aria-describedby="button-addon2">
-                <button class="btn btn-success" type="button" id="button-addon2">
+                <input type="text" class="form-control" wire:model="clientDetails.name" placeholder="Name"
+                    aria-label="Name" aria-describedby="button-addon2">
+                <button class="btn btn-success" type="button" wire:click='storeClient' id="button-addon2">
                     <i class='bx bx-plus-medical'></i>
                 </button>
             </div>
+            @error('clientDetails.name')
+                <label class="text-danger">{{ $message }}</label>
+            @enderror
+            @error('clientDetails.code')
+                <label class="text-danger">{{ $message }}</label>
+            @enderror
             <ul class="list-group">
                 @foreach ($client as $item)
                     <li class="list-group-item d-flex flex-row justify-content-between">
                         <div>{{ strtoupper($item->code) }} - {{ $item->name }}</div>
-                        <button type="button" class="btn btn-sm btn-danger">
+                        <button type="button" class="btn btn-sm btn-danger" wire:click='destroyClient({{ $item->id }})'>
                             <i class='bx bx-window-close'></i>
                         </button>
                     </li>
                 @endforeach
             </ul>
         </div>
-        <div class="col-6">
+        <div class="col-6 d-flex flex-column">
+            <h4>Matter</h4>
             <div class="input-group mb-3">
-                <input type="text" class="form-control" placeholder="Code"
+                <input type="text" class="form-control" wire:model="matterDetails.code" placeholder="Code"
                     aria-label="Code" aria-describedby="button-addon2">
-                    <input type="text" class="form-control" placeholder="Name"
-                        aria-label="name" aria-describedby="button-addon2">
-                <button class="btn btn-success" type="button" id="button-addon2">
+                <input type="text" class="form-control" wire:model="matterDetails.name" placeholder="Name"
+                    aria-label="name" aria-describedby="button-addon2">
+                <button class="btn btn-success" type="button" id="button-addon2" wire:click='storeMatter'>
                     <i class='bx bx-plus-medical'></i>
                 </button>
             </div>
+            @error('matterDetails.name')
+                <label class="text-danger">{{ $message }}</label>
+            @enderror
+            @error('matterDetails.code')
+                <label class="text-danger">{{ $message }}</label>
+            @enderror
             <ul class="list-group">
                 @foreach ($matter as $item)
-                <li class="list-group-item d-flex flex-row justify-content-between">
-                    <div>{{ strtoupper($item->code) }} - {{ $item->name }}</div>
-                    <button type="button" class="btn btn-sm btn-danger">
-                        <i class='bx bx-window-close'></i>
-                    </button>
-                </li>
+                    <li class="list-group-item d-flex flex-row justify-content-between">
+                        <div>{{ strtoupper($item->code) }} - {{ $item->name }}</div>
+                        <button type="button" class="btn btn-sm btn-danger" wire:click='destroyMatter({{ $item->id }})'>
+                            <i class='bx bx-window-close'></i>
+                        </button>
+                    </li>
                 @endforeach
             </ul>
         </div>
-        <div class="col-4"></div>
     </div>
 </div>
