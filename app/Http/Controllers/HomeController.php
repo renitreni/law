8e258d42818654b1062403a04d0cdc615ca4dcfc
Duhell <ryanvillarma148@gmail.com>
2024-03-12 05:35:10 +0800

    Accept pages name as arguments

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Contracts\View\View;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function __invoke(?string $page = "/"): View
     {
-        return view('welcome');
+        $goto = "";
+
+        switch ($page) {
+            case '/':
+                $goto = view('welcome');
+                break;
+            case 'about':
+                $goto = view('about');
+                break;
+            case 'attorneys':
+                $goto = view('attorneys');
+                break;
+            case 'contact':
+                $goto = view('contact');
+                break;
+            default:
+                $goto = "404 page";
+                break;
+        }
+        return $goto;
     }
 }
