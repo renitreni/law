a3bdf7569c77e08a85a91d6615477030ea636e3b
Duhell <ryanvillarma148@gmail.com>
2024-03-12 05:41:02 +0800

    Partial sections

---
+++ resources/views/includes/section-inquire.blade.php
@@ -0,0 +1,94 @@
+<div class="container mb-4">
+    <div class=" text-center mt-5 ">
+
+        <h1>Inquire Now</h1>
+
+    </div>
+    <div class="row ">
+        <div class="col-lg-8 mx-auto">
+            <div class="card mt-2 mx-auto p-4 bg-light">
+                <div class="card-body bg-light">
+
+                    <div class="container">
+                        <form id="contact-form" role="form">
+
+
+
+                            <div class="controls">
+
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="form_name">Firstname</label>
+                                            <input id="form_name" type="text" name="name" class="form-control"
+                                                required="required">
+
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="form_lastname">Lastname</label>
+                                            <input id="form_lastname" type="text" name="surname" class="form-control"
+                                                required="required">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="form_email">Email</label>
+                                            <input id="form_email" type="email" name="email" class="form-control"
+                                                required="required">
+
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="form_need">Please specify your need</label>
+                                            <select id="form_need" name="need" class="form-control" required="required">
+                                                <option value="" selected disabled>--Select Your Issue--</option>
+                                                <option>Request Invoice for order</option>
+                                                <option>Request order status</option>
+                                                <option>Haven't received cashback yet</option>
+                                                <option>Other</option>
+                                            </select>
+
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <label for="form_message">Message</label>
+                                            <textarea id="form_message" name="message" class="form-control"
+                                                placeholder="Write your inquiry here." rows="4"
+                                                required="required"></textarea>
+                                        </div>
+
+                                    </div>
+
+
+                                    <div class="col-md-12">
+
+                                        <input type="submit" style="background-color: #619af8;color:#fff;" class="btn btn-send  pt-2 btn-block
+                        " value="Send Message">
+
+                                    </div>
+
+                                </div>
+
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+
+            </div>
+            <!-- /.8 -->
+
+        </div>
+        <!-- /.row-->
+
+    </div>
+</div>
