ce25df0dd37306b2720ecba818731e5bd1f92274
Duhell <ryanvillarma148@gmail.com>
2024-03-19 21:20:29 +0800

    update contents

--- resources/views/includes/section-about.blade.php
+++ resources/views/includes/section-about.blade.php
@@ -1,28 +1,50 @@
 <section class="ftco-section ftco-no-pt ftco-no-pb">
     <div class="p-5">
-        <h4 style="font-weight:700;">About Mesharial Meshari Al Humaidi Law Firm</h4>
-        <p>
-            For nearly two decades, <strong>Meshari Al Humaidi Law Firm</strong> has consistently occupied a prominent position in the legal services domain. The firm has achieved remarkable successes and earned recognition from various corporations, government institutions, and esteemed embassies.
-        </p>
-        <p>
-            Our accomplishments arise from a <strong>dedicated commitment</strong> to surpassing client expectations, ensuring they find added value in their interactions with our firm. This commitment is embodied in several core principles embraced by <strong>Meshari Al Humaidi Law Firm</strong>.
-        </p>
-        <p>
-            <strong>Innovate & Differentiate</strong>: We are proud of our customized technical solutions, which allow us to provide legal services with <strong>greater speed and efficiency</strong>. These technologies play a crucial role in automating tasks, enhancing operational effectiveness, and facilitating better communication with our esteemed clients.
-        </p>
-        <p>
-            <strong>Case Monitoring System (CMS)</strong>: Our CMS platform efficiently handles case data, ensuring improved services for our clients.
-        </p>
-        <p>
-            <strong>Intelligent  Lawyer</strong>: A specialized platform meticulously crafted for seamless law firm management. It provides comprehensive features including task management, case archiving, session management, a legal library, and more. These advanced legal digital solutions are designed to enhance our clients’ experience.
-        </p>
-        <p>
-            <strong>Legal Consulting</strong>: Our efficient solution for obtaining legal advice on an hourly basis ensures swift and effective service. Clients can conveniently access consultation outcomes via an online platform at any time and from any location.
-        </p>
+        <div class="row">
+            <div class="col-md-6 d-flex justify-content-center align-items-center">
+                <span style="font-size:1.4rem;font-weight:700;">Consistent for 20+ Years</span>
+            </div>
 
-        <details class="border rounded p-3">
-            <summary> Read More </summary>
-            <div class="p-2">
+            <p style="font-weight: 300;" class="col-md-6">
+                For nearly two decades, <strong>Meshari Al Humaidi Law Firm</strong> has consistently occupied a prominent position in the legal services domain. The firm has achieved remarkable successes and earned recognition from various corporations, government institutions, and esteemed embassies.
+            </p>
+        </div>
+
+        <div style="background-color: rgb(241 245 249);" class="p-4 rounded shadow">
+            <p>
+                Our accomplishments arise from a <strong>dedicated commitment</strong> to surpassing client expectations, ensuring they find added value in their interactions with our firm. This commitment is embodied in several core principles embraced by <strong>Meshari Al Humaidi Law Firm</strong>.
+            </p>
+        </div>
+        <div style="margin-top: 3rem;" class="row">
+            <div  class="col-md-6 d-flex justify-content-center align-items-center">
+                <img src="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="img-fluid rounded shadow" style="width:100%;height:300px;">
+            </div>
+
+            <div class="col-md-6">
+                <p >
+                    <strong>Innovate & Differentiate</strong>: We are proud of our customized technical solutions, which allow us to provide legal services with <strong>greater speed and efficiency</strong>. These technologies play a crucial role in automating tasks, enhancing operational effectiveness, and facilitating better communication with our esteemed clients.
+                </p>
+                <p>
+                    <strong>Case Monitoring System (CMS)</strong>: Our CMS platform efficiently handles case data, ensuring improved services for our clients.
+                </p>
+            </div>
+        </div>
+
+        <div style="background-color: rgb(241 245 249);margin-top:3rem;" class="p-4 rounded">
+            <p>
+                <strong>Intelligent  Lawyer</strong>: A specialized platform meticulously crafted for seamless law firm management. It provides comprehensive features including task management, case archiving, session management, a legal library, and more. These advanced legal digital solutions are designed to enhance our clients’ experience.
+            </p>
+            <p>
+                <strong>Legal Consulting</strong>: Our efficient solution for obtaining legal advice on an hourly basis ensures swift and effective service. Clients can conveniently access consultation outcomes via an online platform at any time and from any location.
+            </p>
+        </div>
+
+        <div style="margin-top: 3rem;" class="row">
+            <div  class="col-md-6 d-flex justify-content-center align-items-center">
+                <img src="https://images.unsplash.com/photo-1507679799987-c73779587ccf?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="img-fluid shadow rounded" style="width:100%;height:400px;">
+            </div>
+
+            <div class="col-md-6">
                 <p>
                     Meshari Al Humaidi Law Firm remains steadfast in serving clients, upholding legal standards, and protecting their rights and interests.
                 </p>
@@ -38,33 +60,33 @@
                     Our expert legal guidance helps clients navigate intricate legal situations and resolve contentious matters.
                 </p>
             </div>
-        </details>
+        </div>
     </div>
-    <div class="p-5">
+    <div style="background-color: rgb(241 245 249);margin-top:3rem;" class="p-5">
         <div class="row">
             <div class=" col-md-4">
-                <div class="image-box">
-                    <img class="img-fluid" src="https://images.unsplash.com/photo-1571624436279-b272aff752b5?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Riyadh">
+                <div class="image-box shadow">
+                    <img class="img-fluid rounded" src="https://images.unsplash.com/photo-1571624436279-b272aff752b5?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Riyadh">
                 </div>
                 <div class=" mt-4">
                     <span class="text-xl">Riyadh Head Office</span>
                 </div>
             </div>
             <div class=" col-md-4">
-                <div class="image-box">
-                    <img  class="img-fluid" src="https://images.unsplash.com/photo-1462826303086-329426d1aef5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Hafar">
+                <div class="image-box shadow">
+                    <img  class="img-fluid rounded" src="https://images.unsplash.com/photo-1462826303086-329426d1aef5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Hafar">
                 </div>
                 <div class=" mt-4">
-                    <span class="text-xl">Hafar Albatin</span>
+                    <span class="text-xl">Hafar Albatin Branch</span>
                 </div>
             </div>
 
             <div class=" col-md-4">
-                <div class="image-box">
-                    <img  class="img-fluid" src="https://images.unsplash.com/photo-1542089363-bba089ffaa25?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Al Jubail">
+                <div class="image-box shadow">
+                    <img  class="img-fluid rounded" src="https://images.unsplash.com/photo-1542089363-bba089ffaa25?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Al Jubail">
                 </div>
                 <div class=" mt-4">
-                    <span class="text-xl">Al Jubail</span>
+                    <span class="text-xl">Al Jubail Branch</span>
                 </div>
             </div>
         </div>
