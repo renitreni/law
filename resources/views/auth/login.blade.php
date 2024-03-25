60cead951a8bdcc51addd45ae02f2dc8d71950fd
Duhell <ryanvillarma148@gmail.com>
2024-03-25 18:55:21 +0800

    chamge image

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,8 +24,8 @@
         <div class="container-fluid h-custom">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-md-9 col-lg-6 col-xl-5">
-                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
-                        class="img-fluid" alt="Sample image">
+                    <img style="width:300px;" src="{{ asset('images/icon.png') }}"
+                        class="img-fluid h-50" alt="Sample image">
                 </div>
                 <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
                     <h2>Sign in</h2>
