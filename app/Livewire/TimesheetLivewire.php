daf27c9fd74965f10e89f671f8a26ef5bd5e2de3
Reneir <renier.trenuela@gmail.com>
2024-03-29 23:16:19 +0800

    Fix alpinejs

--- app/Livewire/TimesheetLivewire.php
+++ app/Livewire/TimesheetLivewire.php
@@ -134,25 +134,26 @@ class TimesheetLivewire extends Component
         $this->dispatch('bindMatter', null);
         $this->dispatch('bindOffice', null);
     }
-
+    
+    #[On('keywordClient')]
     public function keywordClient($value)
     {
         $this->timeEntry['client_id'] = $value;
     }
 
-    #[On('keywordClient')]
+    #[On('keywordMatter')]
     public function keywordMatter($value)
     {
         $this->timeEntry['sub_matter_id'] = $value;
     }
 
-    #[On('keywordClient')]
+    #[On('keywordOffice')]
     public function keywordOffice($value)
     {
         $this->timeEntry['office_id'] = $value;
     }
 
-    #[On('keywordClient')]
+    #[On('keywordTemplate')]
     public function keywordTemplate($value)
     {
         $entry = Entry::find($value);
