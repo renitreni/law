ae13d2e564adc3677e104b59c070706aa569fab1
Duhell <ryanvillarma148@gmail.com>
2024-04-01 02:58:46 +0800

    Added models for case

---
+++ app/Models/ClientOpponent.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class ClientOpponent extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'law_cases_id',
+        'name',
+        'contact',
+        'age',
+        'birthday',
+        'company',
+        'role',
+        'attorney'
+    ];
+
+    public function case() : BelongsTo
+    {
+        return $this->belongsTo(LawCase::class,'law_cases_id','id');
+    }
+}
---
+++ app/Models/LawCase.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class LawCase extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'case_title',
+        'case_description',
+        'case_category',
+        'case_status',
+        'case_attorney',
+        'case_date'
+    ];
+
+    public function client() : HasOne
+    {
+        return $this->hasOne(CaseClient::class,'law_cases_id','id');
+    }
+
+    public function opponent() : HasOne
+    {
+        return $this->hasOne(ClientOpponent::class,'law_cases_id','id');
+    }
+
+    public function court() : HasMany
+    {
+        return $this->hasMany(CaseCourt::class,'law_cases_id','id');
+    }
+}
