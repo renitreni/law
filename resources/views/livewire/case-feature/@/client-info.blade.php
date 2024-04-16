f8b877664d7aaf25b85de4581cc5286b85fe9c9c
Duhell <ryanvillarma148@gmail.com>
2024-04-16 17:41:31 +0800

    Add arabic to the form

--- resources/views/livewire/case-feature/@/client-info.blade.php
+++ resources/views/livewire/case-feature/@/client-info.blade.php
@@ -2,41 +2,41 @@
     <h3>Client Information</h3>
     <div class="row mt-4">
         <div class="mb-3 col-md-4">
-            <label for="client_name" class="form-label">Full Name</label>
+            <label for="client_name" class="form-label">Full Name/الاسم الكامل</label>
             <input wire:model='addForm.client_name' type="text" class="form-control" id="client_name" required>
             @error('addForm.client_name') <small class="text-danger"><em>{{ $message }}</em></small> @enderror
 
         </div>
         <div class="mb-3 col-md-4">
-            <label for="client_contact" class="form-label">Contact</label>
+            <label for="client_contact" class="form-label">Contact/الاتصال</label>
             <input wire:model='addForm.client_contact' type="text" class="form-control" id="client_contact" required>
             @error('addForm.client_contact') <small class="text-danger"><em>{{ $message }}</em></small> @enderror
 
         </div>
 
         <div class="mb-3 col-md-2">
-            <label for="client_age" class="form-label">Age</label>
+            <label for="client_age" class="form-label">Age/العمر</label>
             <input wire:model='addForm.client_age' type="text" class="form-control" id="client_age" required>
             @error('addForm.client_age') <small class="text-danger"><em>{{ $message }}</em></small> @enderror
 
         </div>
 
         <div class="mb-3 col-md-2">
-            <label for="client_birthday" class="form-label">Birthdate</label>
+            <label for="client_birthday" class="form-label">Birthdate/تاريخ الميلاد</label>
             <input wire:model='addForm.client_birthday' type="date" class="form-control" id="client_birthday" required>
             @error('addForm.client_birthday') <small class="text-danger"><em>{{ $message }}</em></small> @enderror
 
         </div>
 
         <div class="mb-3 col-md-4">
-            <label for="client_company" class="form-label">Company</label>
+            <label for="client_company" class="form-label">Company/شركة</label>
             <input wire:model='addForm.client_company' type="text" class="form-control" id="client_company" required>
             @error('addForm.client_company') <small class="text-danger"><em>{{ $message }}</em></small> @enderror
 
         </div>
 
         <div class="mb-3 col-md-4">
-            <label for="client_role" class="form-label">Role</label>
+            <label for="client_role" class="form-label">Role/الدور</label>
             <input wire:model='addForm.client_role' list="role" type="text" class="form-control" id="client_role" required>
             @error('addForm.client_role') <small class="text-danger"><em>{{ $message }}</em></small> @enderror
 
@@ -47,7 +47,7 @@
         </div>
 
         <div class="mb-3 col-md-4">
-            <label for="client_attorney" class="form-label">Attorney</label>
+            <label for="client_attorney" class="form-label">Attorney/محامي</label>
             <input wire:model='addForm.client_attorney' list="attorneys" type="text" class="form-control" id="client_attorney" required>
             @error('addForm.client_attorney') <small class="text-danger"><em>{{ $message }}</em></small> @enderror
 
--- resources/views/livewire/case-feature/@/court-info.blade.php
+++ resources/views/livewire/case-feature/@/court-info.blade.php
@@ -3,21 +3,21 @@
     <div class="row mt-4">
 
         <div class="mb-3 col-md-4">
-            <label for="court_name" class="form-label">Court Name</label>
+            <label for="court_name" class="form-label">Court Name/اسم المحكمة</label>
             <input wire:model='addForm.court_name' type="text" class="form-control" id="court_name" required>
             @error('addForm.court_name') <small class="text-danger"><em>{{ $message }}</em></small> @enderror
 
         </div>
 
         <div class="mb-3 col-md-4">
-            <label for="court_address" class="form-label">Address</label>
+            <label for="court_address" class="form-label">Address/العنوان</label>
             <input wire:model='addForm.court_address' type="text" class="form-control" id="court_address" required>
             @error('addForm.court_address') <small class="text-danger"><em>{{ $message }}</em></small> @enderror
 
         </div>
 
         <div class="mb-3 col-md-4">
-            <label for="court_date" class="form-label">Date</label>
+            <label for="court_date" class="form-label">Date/تاريخ</label>
             <input wire:model='addForm.court_date' type="date" class="form-control" id="court_date" required>
             @error('addForm.court_date') <small class="text-danger"><em>{{ $message }}</em></small> @enderror
 
