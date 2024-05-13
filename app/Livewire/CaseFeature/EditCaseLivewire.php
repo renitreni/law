6cf1a611ffb1c8e9279a129b777f4ebfb6e7cc81
Duhell <ryanvillarma148@gmail.com>
2024-05-14 02:53:38 +0800

    Added functions for files CRUD

--- app/Livewire/CaseFeature/EditCaseLivewire.php
+++ app/Livewire/CaseFeature/EditCaseLivewire.php
@@ -7,19 +7,26 @@ use App\Models\LawCase;
 use Livewire\Component;
 use App\Models\CaseCourt;
 use App\Models\CaseClient;
-use App\Models\ClientOpponent;
 use App\Livewire\Forms\AddCaseForm;
+use App\Models\CaseFiles;
+use Illuminate\Support\Facades\Storage;
+use Livewire\WithFileUploads;
 
 class EditCaseLivewire extends Component
 {
+    use WithFileUploads;
     public AddCaseForm $addForm;
     public array $courts = [];
+    public array $files = [];
     public int $id;
+    public bool $isEdit = false;
+    public $newFiles;
 
     public function mount(int|string $id) : void
     {
-        $case = LawCase::with('client', 'opponent', 'court')->findOrFail($id);
+        $case = LawCase::with('client', 'files', 'court')->findOrFail($id);
         $this->id = $case->id;
+        $this->isEdit = true;
         $this->insertData($case);
     }
 
@@ -30,26 +37,17 @@ class EditCaseLivewire extends Component
 
     protected function insertData(LawCase $case):void
     {
-        $this->addForm->case_title = $case->case_title;
+        $this->addForm->case_plaintiff = $case->case_plaintiff;
+        $this->addForm->case_defendant = $case->case_defendant;
         $this->addForm->case_category = $case->case_category;
         $this->addForm->case_status = $case->case_status;
         $this->addForm->case_description = $case->case_description;
         $this->addForm->case_attorney = $case->case_attorney;
         $this->addForm->case_date = $case->case_date;
-        $this->addForm->client_name = $case->client->name;
-        $this->addForm->client_contact = $case->client->contact;
-        $this->addForm->client_age = $case->client->age;
-        $this->addForm->client_birthday = $case->client->birthday;
-        $this->addForm->client_company = $case->client->company;
-        $this->addForm->client_role = $case->client->role;
-        $this->addForm->client_attorney = $case->client->attorney;
-        $this->addForm->opponent_name = $case->opponent->name;
-        $this->addForm->opponent_contact = $case->opponent->contact;
-        $this->addForm->opponent_age = $case->opponent->age;
-        $this->addForm->opponent_birthday = $case->opponent->birthday;
-        $this->addForm->opponent_company = $case->opponent->company;
-        $this->addForm->opponent_role = $case->opponent->role;
-        $this->addForm->opponent_attorney = $case->opponent->attorney;
+        $this->addForm->client_name = $case->client->client_name;
+        $this->addForm->client_contact = $case->client->client_contact;
+        $this->addForm->client_email = $case->client->client_email;
+        $this->addForm->client_company = $case->client->client_company;
 
         foreach($case->court as $court){
             $this->courts[] = [
@@ -59,12 +57,19 @@ class EditCaseLivewire extends Component
                 'court_date' => $court->court_date
             ];
         }
+
+        foreach($case->files as $file){
+            $this->files[] = [
+                'id' => $file->id,
+                'filename' => $file->filename,
+                'uploaded_by' => $file->uploaded_by
+            ];
+        }
     }
 
     public function saveChanges(int $id){
         LawCase::findOrFail($id)->update($this->addForm->getCase());
         CaseClient::where('law_cases_id',$id)->update($this->addForm->getClient($id));
-        ClientOpponent::where('law_cases_id',$id)->update($this->addForm->getClient($id));
 
         foreach($this->courts as $court){
             if($court['id'] === "" || empty($court['id']) ){
@@ -77,6 +82,14 @@ class EditCaseLivewire extends Component
         }
         $this->dispatch('updatedCase','Case updated.');
         return redirect()->back();
+    }
+
+    public function deleteFile(CaseFiles $file)
+    {
+        Storage::disk('public')->delete('case/' . $file->filename);
+        $file->delete();
+        $this->dispatch('deleteCase','Successfully deleted a file.');
+        return redirect()->route('edit_case',['id'=>$this->id]);
     }
 
 
@@ -95,5 +108,23 @@ class EditCaseLivewire extends Component
             'court_address' => "",
             'court_date' => Carbon::now()
         ];
+    }
+
+    public function uploadNewFile()
+    {
+        if(!empty($this->newFiles)){
+            foreach($this->newFiles as $file){
+                $filename = $file->getClientOriginalName();
+                $file->storeAs('public/case',$filename);
+                CaseFiles::create([
+                    'law_cases_id' => $this->id,
+                    'filename' => $filename,
+                    'uploaded_by' => auth()->user()->name
+                ]);
+            }
+
+            $this->dispatch('updatedCase','Successfully added a file.');
+            return redirect()->route('edit_case',['id'=>$this->id]);
+        }
     }
 }
